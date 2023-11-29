6ed4f98096b31ff72d24b870371f8a64fab8f534
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2023-11-29 20:46:37 +0000

    trabajo Guardado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //Funcion Generador de Iconos Para la parte de Arriba
+  let CardIcons = ["♦", "♥", "♠", "♣"];
+
+  function getRandomCard() {
+    return Math.floor(Math.random() * CardIcons.length);
+  }
+
+  function randomCard() {
+    let randomIndex = getRandomCard();
+    let randomCardIcon = CardIcons[randomIndex];
+    document.querySelector("#randomIcon1").innerHTML = randomCardIcon;
+    document.querySelector("#randomIcon2").innerHTML = randomCardIcon;
+  }
+
+  randomCard();
+  //Ahora vamos a crear una funcion que me de de manera random el numero de enmedio!
+  function getRandomNumber() {
+    return Math.ceil(Math.random() * 12);
+  }
+  let number = document.getElementById("number");
+  number.innerHTML = getRandomNumber();
+  //Ahora tenemos que cambiar de manera random los colores de los iconos de carta
 };
