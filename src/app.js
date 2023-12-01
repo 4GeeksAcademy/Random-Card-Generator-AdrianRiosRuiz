19ecfa9cdc6d79be0b3918b724454b3f74e0bb7d
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2023-12-01 18:35:01 +0000

    Retoques

--- src/app.js
+++ src/app.js
@@ -8,22 +8,20 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   //Funcion Generador de Iconos Para la parte de Arriba
-  let CardIcons = ["♦", "♥", "♠", "♣"];
+  let CardIcons = ["♦", "♥", "♠", "♣", "♦", "♥", "♠", "♣"];
   //Con este evento lo que hare sera llamar a toda la funcion cuando el boton sea clickado
   document.querySelector("#boton").addEventListener("click", function() {
-    //Esta funcion te da un icono random
     function getRandomCard() {
       return Math.floor(Math.random() * CardIcons.length);
     }
-    // esta funcion te prueba a ver si los numeros son pares o no para cambiar el color de los iconos pd:No sabia como hacerlo de otra manera
+
     function randomCard() {
       let randomIndex = getRandomCard();
       let randomCardIcon = CardIcons[randomIndex];
       let isEven = randomIndex % 2 === 0;
 
-      randomCardIcon =
-        document.querySelector("#randomIcon1").innerHTML +
-        document.querySelector("#randomIcon2").innerHTML;
+      document.querySelector("#randomIcon1").innerHTML = randomCardIcon;
+      document.querySelector("#randomIcon2").innerHTML = randomCardIcon;
 
       if (isEven) {
         document.querySelector("#randomIcon1").style.color = "red";
@@ -37,7 +35,7 @@ window.onload = function() {
     }
 
     randomCard();
-    //Ahora vamos a crear una funcion que me de de manera random el numero de enmedio o una letra!
+
     function getRandomNumeroYletra() {
       let numerosYletras = [
         "1",
@@ -58,6 +56,7 @@ window.onload = function() {
       );
       return numerosYletras[randomNumerosYletrasResult];
     }
+
     let number = document.querySelector("#number");
     number.innerHTML = getRandomNumeroYletra();
   });
